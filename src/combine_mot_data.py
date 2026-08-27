031eb5bada9e4cfea489021e446f85fccb81ccc3
Nasser-Sanchez <jameel02@live.co.uk>
2026-08-27 21:08:16 +0100

    filter data in combined query

--- src/combine_mot_data.py
+++ src/combine_mot_data.py
@@ -5,7 +5,15 @@ con = duckdb.connect("uk_car_analyser.duckdb")
 query = """
     SELECT *
     FROM read_parquet('data/mot_data_*.parquet')
-    WHERE first_use_date IS NOT NULL
+    WHERE first_use_date>='2000-01-01'
+        AND test_mileage IS NOT NULL
+        AND(
+
+            (cylinder_capacity BETWEEN 799 AND 7000) 
+            OR fuel_type='EL'    
+        )
+        AND model<>'UNCLASSIFIED'
+        
 """
 
 # Save the result
