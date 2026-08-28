b726a4e2f5444653ad693e82e6a7850780affe87
Nasser-Sanchez <jameel02@live.co.uk>
2026-08-28 01:16:06 +0100

    filter for cars >=0.95L displacement and remove filter on pre 2000 cars

--- src/combine_mot_data.py
+++ src/combine_mot_data.py
@@ -5,14 +5,14 @@ con = duckdb.connect("uk_car_analyser.duckdb")
 query = """
     SELECT *
     FROM read_parquet('data/mot_data_*.parquet')
-    WHERE first_use_date>='2000-01-01'
-        AND test_mileage IS NOT NULL
+    WHERE test_mileage IS NOT NULL
         AND(
 
-            (cylinder_capacity BETWEEN 799 AND 7000) 
+            (cylinder_capacity BETWEEN  950 AND 7000) 
             OR fuel_type='EL'    
         )
         AND model<>'UNCLASSIFIED'
+        AND first_use_date<=test_date
         
 """
 
