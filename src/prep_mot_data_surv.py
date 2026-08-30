7ccce79ba44d37caf2c94b19a943472f3e3ce12e
Nasser-Sanchez <jameel02@live.co.uk>
2026-08-30 22:42:40 +0100

    Adapt survival data prep for api data

--- src/prep_mot_data_surv.py
+++ src/prep_mot_data_surv.py
@@ -2,24 +2,43 @@ import duckdb
 con = duckdb.connect("uk_car_analyser.duckdb")
 con.execute("""
     COPY(
-        WITH last_test_prep AS(
-           SELECT vehicle_id, (DATE_DIFF('day', first_use_date, test_date))/365.25 AS t, 
-           IF(test_result IN ('ABA', 'F'), 1, 0) AS event,
+        WITH max_date AS(
+            SELECT MAX(CAST(test_completedDate AS DATE)) AS last_mot_date
+            FROM read_parquet('data/mot_data_combined.parquet')
+        ),
+        
+        last_test_prep AS(
+           SELECT registration, firstUsedDate, years, mileage, 
+           event,
            IF(
-               test_result NOT IN ('ABA', 'F') AND test_date<'2024-12-31',
+               event<>1 AND DATE_ADD(test_completedDate, INTERVAL 13 MONTH)<m.last_mot_date,
                1,
                0
             ) AS interval_censored,
+            IF(
+                interval_censored=1,
+                mileage+15000,
+                mileage
+            ) AS mileage_interval,
+            IF(
+                interval_censored=1,
+                years+(13/12),
+                years
+            ) AS years_interval,
+            
            make, model, 
-           fuel_type, cylinder_capacity, test_mileage,
-           first_use_date AS start_date, test_date AS end_date,
-           ROW_NUMBER()OVER(PARTITION BY vehicle_id ORDER BY test_date DESC, test_id DESC) AS rownum
+           fuelType, engineSize,
+           SUM(defect_count_advisory)OVER(PARTITION BY vehicle_id) AS defect_count_advisory,
+           SUM(defect_count_dangerous)OVER(PARTITION BY vehicle_id) AS defect_count_dangerous,
+           firstusedDate AS start_date, CAST(test_completedDate AS DATE) AS end_date, last_test,
+           1 AS event_interval
            
-           FROM read_parquet('data/mot_data_combined.parquet')
-           WHERE first_use_date<test_date
+           FROM read_parquet('data/mot_data_combined.parquet') d
+           JOIN max_date m ON 1=1
+           
            
         )
     
-        SELECT * EXCLUDE(rownum) FROM last_test_prep WHERE rownum=1
+        SELECT * EXCLUDE(last_test) FROM last_test_prep WHERE last_test=1 AND mileage_interval<3000000 AND NOT isinf(mileage_interval)
     ) TO 'data/mot_last_test.parquet' (FORMAT PARQUET); 
 """)
